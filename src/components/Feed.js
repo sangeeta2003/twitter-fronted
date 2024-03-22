2928d8a3967be51c0ef3f6c1643eca0651487e19
*pknowledge* <mishrasangeeta944@gmail.com>
2024-03-22 11:20:11 +0530

    tweet.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Post from './Post'
+import Tweet from './Tweet'
 
 const Feed = () => {
   return (
-    <div className="w-[50%] border border-gray-600">
+    <div className="w-[50%] border border-gray-800">
       <div>
         <Post />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
       </div>
     </div>
   )
